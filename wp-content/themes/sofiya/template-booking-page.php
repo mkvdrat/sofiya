f54bfc74f043abbc81aa46b3951813e02670dc9d
Server <q3dm0@yahoo.com>
2019-12-26 09:26:28 +0300

    Страница бронирования

--- wp-content/themes/sofiya/template-booking-page.php
+++ wp-content/themes/sofiya/template-booking-page.php
@@ -21,26 +21,12 @@ get_header();
             </div>
         </div>
 
-		<?php echo get_field('description_standart_page'); //Удалить строку и Сюда верстать?>
-
-        <?php
-            $args = array(
-                'numberposts' => '3',
-                'category'    => '11,14',
-                'post_type'   => 'post',
-                'orderby'     => 'date',
-                'order'       => 'DESC',
-            );
-
-            $lists = get_posts( $args );
-        ?>
 		<div class="main__text">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        <div class="section__title">Онлайн-бронирование</div>
-                        <div class="section__description">Заполните форму для брони номера
-                        </div>
+                        <div class="section__title"><?php echo get_field('title_online_booking_page'); ?></div>
+                        <div class="section__description"><?php echo get_field('subtitle_online_booking_page'); ?></div>
 						<div class="btn-group-back btn__groupBron">
                             <a href="#to__form" class="btn btn__link scrollto">ОНЛАЙН БРОНИРОВАНИЕ</a>
                             <a href="#" class="btn btn__link" data-toggle="modal" data-target="#easyBron">ЛЁГКОЕ БРОНИРОВАНИЕ</a>
@@ -53,45 +39,12 @@ get_header();
 											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 												<span aria-hidden="true">&times;</span>
 											</button>
-											<form>
-												<div class="form__title">Легкое бронирование offline</div>
-												<div class="form-group">
-																	<span>
-																		<input type="text" value="" placeholder="Введите Ваше имя" required/>
-																	</span>
-												</div>
-												<div class="form-group">
-																	<span>
-																		<input type="email" value="" placeholder="Введите e-mail"/>
-																	</span>
-												</div>
-												<div class="form-group">
-																	<span>
-																		<input type="tel" value="" placeholder="7 (___) ___-__-__"/>
-																	</span>
-												</div>
-												<div class="form-group">
-																	<span>
-																		<input type="text"  aria-required="true" aria-invalid="false" placeholder="Дата заезда">
-																	</span>
-												</div>
-												<div class="form-group">
-																	<span>
-																		<input type="text"  aria-required="true" aria-invalid="false" placeholder="Дата выезда">
-																	</span>
-												</div>
-												<div class="form-group">
-																	<span>
-																		<input type="checkbox" value="" id="confirm-2">
-																		<label for="confirm-2">Я даю согласие на обработку моих персональных данных</label>
-																	</span>
-												</div>
-												<div class="form-group">
-																	<span>
-																		<input type="submit" value="Отправить" class="btn btn-submit"/>
-																	</span>
-												</div>
-											</form>
+											<?php
+												$forms_a = get_field('easy_form_booking_page');
+												if($forms_a){
+													echo do_shortcode('[contact-form-7 id=" ' . $forms_a . ' "]'); 
+												}
+											?>
 										</div>
 									</div>
 								</div>
@@ -102,34 +55,21 @@ get_header();
         </div>
 		
         <div class="info__block" id="to__form">
-		<div class="container-fluid">
-                <div class="row row-flex">
-                    <div class="col-xs-12">
-		<div class="form--out"><img src="/wp-content/themes/sofiya/image/form.png"/></div>
-		</div>
-		</div>
-		</div>
+			<div class="container-fluid">
+				<div class="row row-flex">
+					<div class="col-xs-12">
+						<div class="form--out">
+							<?php echo get_field('form_booking_page'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
         <div class="info__block">
 			<div class="container">
                 <div class="row row-flex">
                     <div class="col-xs-12">
-       <blockquote>Планируете свой летний отпуск в Ялте? Получите ГАРАНТИРОВАННУЮ бронь на лучший отдых в Крыму прямо сейчас!
-	   Неожиданно выдались свободные дни? Не теряйте времени и выбирайте номера не откладывая, гарантируя своим близким идеальный отпуск в удобное для вас время и по самой комфортной цене.
-                        </blockquote>
-						<h3>Бронирование отеля онлайн — это легко, быстро и безопасно.</h3>
-                            <div class="text__block">
-							
-								Для оплаты вы можете использовать:
-							<ul>
-								<li>кредитную карту;</li>
-								<li>электронные деньги;</li>
-								<li>безналичный расчет.</li>
-								<li>Также окончательную доплату вы можете сделать наличными или кредитной картой уже в отеле.</li>
-							</ul>
-						
-							</div>
-						</div>
+						<?php echo get_field('text_a_block_online_booking_page'); ?>
 					</div>
 				</div>
 		</div>
@@ -137,26 +77,14 @@ get_header();
             <div class="container">
                 <div class="row row-flex">
                     <div class="col-xs-12 col-sm-4">
-                        <?php $image_url = get_field('image_attractions_block_main_page'); ?>
+                        <?php $image_url = get_field('image_online_booking_page'); ?>
                         <div class="inline__image"><img src="<?php echo $image_url ? $image_url : esc_url( get_template_directory_uri() ) . '/image/no_image.jpg'; ?>"/></div>
                     </div>
                     <div class="col-xs-12 col-sm-8">
                         <div class="text__container">
-                            <h3>Бронирование отеля онлайн — это легко, быстро и безопасно.</h3>
-                            <div class="text__block">
+                            <?php echo get_field('text_b_block_online_booking_page'); ?>
 							
-								Отель «Премиум Парк» принимает заявки на бронирование номеров:
-							<ul>
-								<li>по телефонам: +7 (978) 924-33-99 | +7 (978) 061 01 94</li>
-								<li>через форму бронирования on-line</li>
-								<li>по электронной почте: PremiumPark.yalta@yandex.ru</li>
-								<li>от имени компании (предприятия, профсоюза, организации …),</li>
-								<li>через турагентство (туроператорскую фирму).</li>
-								<li>от турагентств: +7 (978) 924-33-99</li>
-							</ul>
-                                <?php echo get_field('text_attractions_block_main_page'); ?>
-                            </div>
-                            <a href="<?php echo get_field('link_attractions_block_main_page'); ?>" class="btn btn__link">ПОДРОБНЕЕ</a>
+                            <a href="<?php echo get_field('link_online_booking_page'); ?>" class="btn btn__link">ПОДРОБНЕЕ</a>
                         </div>
                     </div>
                 </div>
@@ -168,10 +96,7 @@ get_header();
                 <div class="row row-flex">
                     <div class="col-xs-12">
                         <div class="text__container">
-							<h3>Условия бронирования</h3>
-							<p>Бронирование считается гарантированным только при получении предоплаты в размере 50% оплаты стоимости проживания на основании выставленного счета.</p>
-							<p><i>*Обратите внимание: При аннуляции гарантированного бронирования менее чем за 7 суток до даты предполагаемого заезда предоплата в размере стоимости 1 суток проживания гостю не возвращается.</i></p>
-							<p><i>**Предварительное бронирование не является гарантированным бронированием.</i></p>
+							<?php echo get_field('text_c_block_online_booking_page'); ?>
                         </div>
                     </div>
                 </div>
